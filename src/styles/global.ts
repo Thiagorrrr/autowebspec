4105339d3884f8572270dbbdc80103c3b1f28cbd
Thiago Pereira <thiagopereira@MacBook-Pro-de-Thiago.local>
2025-11-21 23:35:35 -0300

    new css

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -4,11 +4,11 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyles = createGlobalStyle`
 
 
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
+// * {
+//   margin: 0;
+//   padding: 0;
+//   box-sizing: border-box;
+// }
 
 html {
   font-size: 62.5%;
